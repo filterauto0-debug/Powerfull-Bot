139150e2e4602ca59dbd37a05e953f26b10ce698
filterauto0-debug <filterauto0@gmail.com>
2026-07-25 20:52:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -61,8 +61,8 @@ OWNER_UPI_ID = environ.get('OWNER_UPI_ID', '')
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filterauto0_db_user:<dhanush8996>@sdmovieadda.soojroo.mongodb.net/?appName=SDmovieADDA")
-DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://filterauto0_db_user:<dhanush8996>@sdmovieadda.soojroo.mongodb.net/?appName=SDmovieADDA")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://filterauto0_db_user:Dhanu8996Bot123@sdmovieadda.soojroo.mongodb.net/filterauto0_db_user?retryWrites=true&w=majority&appName=SDmovieADDA")
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://filterauto0_db_user:Dhanu8996Bot123@sdmovieadda.soojroo.mongodb.net/filterauto0_db_user?retryWrites=true&w=majority&appName=SDmovieADDA")
 DATABASE_NAME = environ.get('DATABASE_NAME', "filterauto0_db_user")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Deendayal_files')
 
